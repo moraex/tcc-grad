15a5ffbdbbd1690c8fcf90bf4d71e8b6d19c04b0
moons2 <moraes.luan1997@gmail.com>
2023-03-03 02:00:15 -0300

    convert DAG to Tree with success

--- experimentos/build_subsets.py
+++ experimentos/build_subsets.py
@@ -3,175 +3,184 @@ import math
 import os
 import sys
 
+
 class ReadFile:
 
-	def read(self, file_path):
-		with open(file_path,"r") as f:
-			return f.readlines()
+    def read(self, file_path):
+        with open(file_path, "r") as f:
+            return f.readlines()
+
 
 class WriteFile:
 
-	def write(self, file_path, data):
-		with open(file_path,"w") as f:
-			f.writelines(''.join(data))
+    def write(self, file_path, data):
+        with open(file_path, "w") as f:
+            f.writelines(''.join(data))
+
 
 class ReliefRankFeatures:
-	_features = []
-	Q1 = 0
-	Q2 = 0
-	Q3 = 0
+    _features = []
+    Q1 = 0
+    Q2 = 0
+    Q3 = 0
 
-	def __init__(self, file):
-		file = ReadFile().read(file)
-		sample = False
-		for line in file:
-			if sample:
-				self._features.append(line.split("\t")[1])
-			if line.count("---------------------------------------------------------------------------------------------------------------------------------------------"):
-				sample = True
+    def __init__(self, file):
+        file = ReadFile().read(file)
+        sample = False
+        for line in file:
+            if sample:
+                self._features.append(line.split("\t")[1])
+            if line.count("-------------"):
+                sample = True
 
-		self.Q3 = math.ceil(0.25 * len(self._features))
-		self.Q2 = math.ceil(0.5 * len(self._features))
-		self.Q1 = math.ceil(0.75 * len(self._features))
+        self.Q3 = math.ceil(0.25 * len(self._features))
+        self.Q2 = math.ceil(0.5 * len(self._features))
+        self.Q1 = math.ceil(0.75 * len(self._features))
 
-	def top25(self):
-		return self._features[:self.Q3]
+    def top25(self):
+        return self._features[:self.Q3]
 
-	def top50(self):
-		return self._features[:self.Q2]
+    def top50(self):
+        return self._features[:self.Q2]
 
-	def top75(self):
-		return self._features[:self.Q1]
+    def top75(self):
+        return self._features[:self.Q1]
 
-	def audit(self):
-		okay = True
-		reason = []
-		if (2*self.Q2 - len(self._features) > 1):
-			okay = False
-			reason.append('2xQ2 > len(features)')
+    def audit(self):
+        okay = True
+        reason = []
+        if (2*self.Q2 - len(self._features) > 1):
+            okay = False
+            reason.append('2xQ2 > len(features)')
 
-		if ((self.Q1+self.Q3) - len(self._features) > 1):
-			okay = False
-			reason.append('Q1+Q3 > len(features)')
+        if ((self.Q1+self.Q3) - len(self._features) > 1):
+            okay = False
+            reason.append('Q1+Q3 > len(features)')
 
-		if not okay:
-			print(f'rasoes: {",".join(reason)}')
-		else:
-			print('APARENTEMENTE OKAY')
+        if not okay:
+            print(f'rasoes: {",".join(reason)}')
+        else:
+            print('APARENTEMENTE OKAY')
+
 
 class BuildFeatureRankedSubset:
-	ReliefFeatures = None
-	source_trainvalid_sataset = None
-	source_prefix = 'raw_datasets/'
-	target_prefix = 'output_relief_method/'
-	dataset_name = None
-	dataset_test = None
+    ReliefFeatures = None
+    source_trainvalid_sataset = None
+    source_prefix = 'raw_datasets/'
+    target_prefix = 'output_relief_method/'
+    dataset_name = None
+    dataset_test = None
 
-	def __init__(self, source_file):
-		self.dataset_name = source_file
-		self.ReliefFeatures = ReliefRankFeatures(
-			self.source_prefix + 
-			source_file + '/' + 
-			source_file + 
-			'.fimp')
-		self.source_trainvalid_sataset = ReadFile().read(
-			self.source_prefix + 
-			source_file + '/' + 
-			source_file + 
-			'.trainvalid.arff')
-		self.dataset_test = ReadFile().read(
-			self.source_prefix + 
-			source_file + '/' + 
-			source_file + 
-			'.test.arff')
-		self.runFile()
-		self.runTest()
+    def __init__(self, source_file):
+        self.dataset_name = source_file
+        self.ReliefFeatures = ReliefRankFeatures(
+            self.source_prefix +
+            source_file + '/' +
+            source_file +
+            '.fimp')
+        self.source_trainvalid_sataset = ReadFile().read(
+            self.source_prefix +
+            source_file + '/' +
+            source_file +
+            '.trainvalid.arff')
+        self.dataset_test = ReadFile().read(
+            self.source_prefix +
+            source_file + '/' +
+            source_file +
+            '.test.arff')
+        self.runFile()
+        self.runTest()
 
-	def keepIndexes(self, original_feature_space, keep_features):
-		return [original_feature_space.index(att) for att in keep_features] + [-1]
+    def keepIndexes(self, original_feature_space, keep_features):
+        return [original_feature_space.index(att) for att in keep_features] + [-1]
 
-	def runFile(self):
-		for quartil in [['Q3', self.ReliefFeatures.top25()],['Q2', self.ReliefFeatures.top50()],['Q1',self.ReliefFeatures.top75()]]:
+    def runFile(self):
+        for quartil in [['Q3', self.ReliefFeatures.top25()], ['Q2', self.ReliefFeatures.top50()], ['Q1', self.ReliefFeatures.top75()]]:
 
-			subset = []
-			curr_sample_idx = 0
-			original_feature_space = []
-			keep_features = []
-			RELATION = False
+            subset = []
+            curr_sample_idx = 0
+            original_feature_space = []
+            keep_features = []
+            RELATION = False
 
-			for line in self.source_trainvalid_sataset:
-				curr_sample_idx += 1
-				if (not RELATION) and line.count("@RELATION"):
-					subset.append(line)
-					subset.append("\n")
-					RELATION = True
+            for line in self.source_trainvalid_sataset:
+                curr_sample_idx += 1
+                if (not RELATION) and line.count("@RELATION"):
+                    subset.append(line)
+                    subset.append("\n")
+                    RELATION = True
 
-				if line.count("@DATA"):
-					subset.append(line)
-					break
+                if line.count("@DATA"):
+                    subset.append(line)
+                    break
 
-				if line.count("@ATTRIBUTE"):
-					original_feature_space.append(line.split(" ")[1])
-					if line.split(" ")[1] in quartil[1]:
-						keep_features.append(line.split(" ")[1])
-						subset.append(line)
+                if line.count("@ATTRIBUTE"):
+                    original_feature_space.append(line.split(" ")[1])
+                    if line.split(" ")[1] in quartil[1]:
+                        keep_features.append(line.split(" ")[1])
+                        subset.append(line)
 
-				if line.count("@ATTRIBUTE class "):
-					subset.append(line)
-					subset.append("\n")
+                if line.count("@ATTRIBUTE class "):
+                    subset.append(line)
+                    subset.append("\n")
 
-			keep_indexes = self.keepIndexes(original_feature_space, keep_features)
+            keep_indexes = self.keepIndexes(
+                original_feature_space, keep_features)
 
-			for line in self.source_trainvalid_sataset[curr_sample_idx:]:
-				new_line = np.array(line.split(","))
-				subset.append(','.join(new_line[keep_indexes].tolist()))
+            for line in self.source_trainvalid_sataset[curr_sample_idx:]:
+                new_line = np.array(line.split(","))
+                subset.append(','.join(new_line[keep_indexes].tolist()))
 
-			# we create target folders if not exists
-			target_folder = f'output_relief_method/{self.dataset_name}/{quartil[0]}/'
-			if not os.path.exists(target_folder):
-				os.makedirs(target_folder)
+            # we create target folders if not exists
+            target_folder = f'output_relief_method/{self.dataset_name}/{quartil[0]}/'
+            if not os.path.exists(target_folder):
+                os.makedirs(target_folder)
 
-			WriteFile().write(f'{target_folder}{self.dataset_name}.trainvalid.arff', subset)
+            WriteFile().write(
+                f'{target_folder}{self.dataset_name}.trainvalid.arff', subset)
 
-	def runTest(self):
-		for quartil in [['Q3', self.ReliefFeatures.top25()],['Q2', self.ReliefFeatures.top50()],['Q1',self.ReliefFeatures.top75()]]:
+    def runTest(self):
+        for quartil in [['Q3', self.ReliefFeatures.top25()], ['Q2', self.ReliefFeatures.top50()], ['Q1', self.ReliefFeatures.top75()]]:
 
-			subset = []
-			curr_sample_idx = 0
-			original_feature_space = []
-			keep_features = []
-			RELATION = False
+            subset = []
+            curr_sample_idx = 0
+            original_feature_space = []
+            keep_features = []
+            RELATION = False
 
-			for line in self.dataset_test:
-				curr_sample_idx += 1
-				if (not RELATION) and line.count("@RELATION"):
-					subset.append(line)
-					subset.append("\n")
-					RELATION = True
+            for line in self.dataset_test:
+                curr_sample_idx += 1
+                if (not RELATION) and line.count("@RELATION"):
+                    subset.append(line)
+                    subset.append("\n")
+                    RELATION = True
 
-				if line.count("@DATA"):
-					subset.append(line)
-					break
+                if line.count("@DATA"):
+                    subset.append(line)
+                    break
 
-				if line.count("@ATTRIBUTE"):
-					original_feature_space.append(line.split(" ")[1])
-					if line.split(" ")[1] in quartil[1]:
-						keep_features.append(line.split(" ")[1])
-						subset.append(line)
+                if line.count("@ATTRIBUTE"):
+                    original_feature_space.append(line.split(" ")[1])
+                    if line.split(" ")[1] in quartil[1]:
+                        keep_features.append(line.split(" ")[1])
+                        subset.append(line)
 
-				if line.count("@ATTRIBUTE class "):
-					subset.append(line)
-					subset.append("\n")
+                if line.count("@ATTRIBUTE class "):
+                    subset.append(line)
+                    subset.append("\n")
 
-			keep_indexes = self.keepIndexes(original_feature_space, keep_features)
+            keep_indexes = self.keepIndexes(
+                original_feature_space, keep_features)
 
-			for line in self.dataset_test[curr_sample_idx:]:
-				new_line = np.array(line.split(","))
-				subset.append(','.join(new_line[keep_indexes].tolist()))
+            for line in self.dataset_test[curr_sample_idx:]:
+                new_line = np.array(line.split(","))
+                subset.append(','.join(new_line[keep_indexes].tolist()))
 
-			target_folder = f'output_relief_method/{self.dataset_name}/{quartil[0]}/'
-			WriteFile().write(f'{target_folder}{self.dataset_name}.test.arff', subset)
+            target_folder = f'output_relief_method/{self.dataset_name}/{quartil[0]}/'
+            WriteFile().write(
+                f'{target_folder}{self.dataset_name}.test.arff', subset)
+
 
 if __name__ == '__main__':
-	dataset = sys.argv[1]
-	BuildFeatureRankedSubset(dataset)
+    dataset = sys.argv[1]
+    BuildFeatureRankedSubset(dataset)
---
+++ experimentos/dag2tree.py
@@ -0,0 +1,155 @@
+import networkx as nx
+import os
+import sys
+
+
+class ReadFile:
+
+    def read(self, file_path):
+        with open(file_path, "r") as f:
+            return f.readlines()
+
+
+class WriteFile:
+
+    def write(self, file_path, data):
+        with open(file_path, "w") as f:
+            f.writelines(''.join(data))
+
+
+class OSPath:
+    path = None
+    file_name = None
+    file_extension = None
+    full = None
+
+    def __init__(self, full_path) -> None:
+        self.full = full_path
+        self.path, file = os.path.split(full_path)
+        self.file_name = file.split(".")[0]
+        self.file_extension = ".".join(file.split(".")[1:])
+
+
+class Graph:
+    T = None
+    DAG = None
+    leaves = None
+
+    def __init__(self, edges) -> None:
+        self.DAG = nx.DiGraph()
+        self.DAG.add_edges_from(edges)
+        # construo uma arvore a partir de um DAG
+        self.T = nx.dfs_tree(self.DAG, source="root")
+        self.leaves = [v for v, d in self.T.out_degree() if d == 0]
+
+    def antecessors(self, node):
+        """retorna um vetor com todos os antecessores
+        do nó node até a raiz da arvore T"""
+        q = [node]
+        paths = []
+        while q:
+            node = q.pop(0)
+            paths.append(node)
+            if not self.T.pred[node]:
+                break
+            q.append(list(self.T.pred[node])[0])
+        return paths
+
+    def antecessors2str(self, antecessors):
+        """ recebe um vetor de antecessores e 
+        retorna no formato 'root/class_0/class_1/.../class_n'"""
+        return '/'.join(antecessors.__reversed__())
+
+    def treeSpace2Str(self):
+        """retorna todas as combinações de caminhos possiveis de uma árvore"""
+        paths = nx.all_simple_paths(self.T, source="root", target=self.leaves)
+        output_tree_space = []
+        for path in list(paths):
+            p = path
+            while p:
+                output_tree_space.append('/'.join(p))
+                p.pop(-1)
+        return ",".join(set(output_tree_space))
+
+    def parseManyClasses2Tree(self, string):
+        """recebe uma string de DAG classes e retorna a correspondencia em Tree"""
+        target_sample_space = []
+        sample_classes = list(string.split("@"))
+        target_sample_space = [self.antecessors2str(
+            self.antecessors(c)) for c in sample_classes]
+        return '@'.join(target_sample_space)
+
+
+class ParseArff:
+    file_attr_space = []
+    class_attr_space = []
+    data_indicator_line = []
+    data_space = []
+    _Graph = None
+    _OSPath = None
+
+    def __init__(self, file_arff) -> None:
+        self._OSPath = OSPath(file_arff)
+        file = ReadFile().read(self._OSPath.full)
+        self.runFile(file)
+        self.initEdges(self.class_attr_space[0].split(" ")[-1])
+        self.reshapeClassSpace()
+        self.reshapeSampleClassSpace()
+        self.generateTreeFile(
+            f"{self._OSPath.path}/{self._OSPath.file_name}.trainvalid.fake.arff")
+
+    def runFile(self, file):
+        curr_sample_idx = 0
+        relation = False
+        class_idx_line = 0
+
+        for line in file:
+            curr_sample_idx += 1
+            if (not relation) and line.count("@RELATION"):
+                self.file_attr_space.append(line)
+                self.file_attr_space.append("\n")
+                relation = True
+
+            if line.count("@DATA"):
+                self.data_indicator_line.append("\n")
+                self.data_indicator_line.append(line)
+                break
+
+            if line.count("@ATTRIBUTE class "):
+                self.class_attr_space.append(line)
+                class_idx_line = curr_sample_idx
+            elif line.count("@ATTRIBUTE "):
+                self.file_attr_space.append(line)
+
+        for line in file[curr_sample_idx:]:
+            if line.replace("\n", "") != "":
+                self.data_space.append(line)
+
+    def initEdges(self, dag_string_space):
+        dag_string = dag_string_space.replace("\n", "")
+        edges = [tuple(v.split("/")) for v in dag_string.split(",")]
+        self._Graph = Graph(edges)
+
+    def reshapeClassSpace(self):
+        new_class_attr_space = self.class_attr_space[0].split(" ")
+        new_class_attr_space[-1] = self._Graph.treeSpace2Str() + "\n"
+        self.class_attr_space = [" ".join(new_class_attr_space)]
+
+    def reshapeSampleClassSpace(self):
+        new_data_space = []
+        for line in self.data_space:
+            arr_line = line.replace("\n", "").split(",")
+            arr_line[-1] = self._Graph.parseManyClasses2Tree(arr_line[-1])
+            new_line = ','.join(arr_line) + '\n'
+            new_data_space.append(new_line)
+        self.data_space = new_data_space
+
+    def generateTreeFile(self, file_name):
+        output = self.file_attr_space + self.class_attr_space + \
+            self.data_indicator_line + self.data_space
+        WriteFile().write(file_name, output)
+
+
+if __name__ == '__main__':
+    dataset = sys.argv[1]
+    ParseArff(dataset)
