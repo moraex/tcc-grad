c1061e99da78182b1ecc6e6bf5a91fa88a864c64
moons2 <moraes.luan1997@gmail.com>
2023-03-05 03:48:25 -0300

    output for methods added

--- experimentos/run-clus.sh
+++ experimentos/run-clus.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-#for f in $(ls raw_datasets); do
-#	if [ -d "raw_datasets/$f" ]; then
-#		for d in $(ls "output_relief_method/$f/"); do
-#			DESTIN="output_relief_method/$f/$d/$f.s"
-#			java -jar clus.jar $DESTIN
-#		done
-#	fi
-#done
-
 source_folder=$1
 
 for f in $(ls $source_folder); do
 	if [ -d "$source_folder/$f" ]; then
-		DESTIN="$source_folder/$f/$f.s"
-		sed -i "s/%%//" $DESTIN && java -jar clus.jar $DESTIN
+		for d in $(ls "$source_folder/$f"); do
+			echo "=== DOING $f... ==="
+			DESTIN="$source_folder/$f/$d/$f.s"
+			java -jar clus.jar $DESTIN
+			echo "=== DONE $f ==="
+		done
 	fi
 done
+
+
+
+#for f in $(ls $source_folder); do
+#	if [ -d "$source_folder/$f" ]; then
+#		DESTIN="$source_folder/$f/$f.s"
+#		sed -i "s/%%//" $DESTIN && java -jar clus.jar $DESTIN
+#	fi
+#done
